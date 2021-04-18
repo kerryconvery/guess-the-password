035cac777ef2cd142872bdca6b83f5c10bfcce39
Kerry Convery <kerry.convery@myob.com>
2021-04-18 10:24:42 +1000

    added remaining unit tests

--- guess-the-password-ui/src/guess-the-password-page/__tests__/CharacterList.test.js
+++ guess-the-password-ui/src/guess-the-password-page/__tests__/CharacterList.test.js
@@ -4,12 +4,53 @@ import CharacterList from '../CharacterList';
 import '@testing-library/jest-dom/extend-expect';
 
 describe('When rendering characters', () => {
-  const characters = ['1', '2'];
+  const characters = ['1', '2', '3'];
 
   it('should display all characters', () => {
     render(<CharacterList characters={characters} />);
 
     expect(screen.getByText('1')).toBeInTheDocument();
     expect(screen.getByText('2')).toBeInTheDocument();
+    expect(screen.getByText('3')).toBeInTheDocument();
+  });
+
+  it('should highlight highlighted characters in green', () => {
+    const highlightedCharacters = ['2'];
+
+
+    render(<CharacterList characters={characters} highlightedCharacters={highlightedCharacters}/>);
+
+    const highlightedCharacter = screen.getByText('2');
+
+    expect(highlightedCharacter).toHaveStyle('background-color: green');
+    expect(highlightedCharacter).toHaveStyle('color: white');
+  });
+
+  it('should not highlight non highlighted characters', () => {
+    const highlightedCharacters = ['2'];
+
+
+    render(<CharacterList characters={characters} highlightedCharacters={highlightedCharacters}/>);
+
+    const highlightedCharacter = screen.getByText('1');
+
+    expect(highlightedCharacter).toHaveStyle('background-color: white');
+    expect(highlightedCharacter).toHaveStyle('color: black');
+  });
+
+  it('should render small size characters', () => {
+    render(<CharacterList characters={characters} size='small' />);
+
+    const character = screen.getByText('1');
+
+    expect(character).toHaveStyle('font-size: 1em')
+  });
+
+  it('should render medium size characters', () => {
+    render(<CharacterList characters={characters} size='medium' />);
+
+    const character = screen.getByText('1');
+
+    expect(character).toHaveStyle('font-size: 2em')
   });
 })
